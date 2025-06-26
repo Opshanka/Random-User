0a9b82f6e3da40dcb4287b5be064de298ca78159
Opshanka <opibabeykoon@gmail.com>
2025-06-26 17:39:50 +0530

    Initial Commit

---
+++ Random User/Random_UserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_UserApp.swift
+//  Random User
+//
+//  Created by Opshanka Prabath on 2025-06-26.
+//
+
+import SwiftUI
+
+@main
+struct Random_UserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
